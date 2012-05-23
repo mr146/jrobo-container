b01ce43781c983147231f5126e8ae067c0ce70e0
Den.Mukhametianov@gmail.com <Den.Mukhametianov@gmail.com@0c7f8088-1b77-78cd-0a42-4bde29f108f7>
2012-05-23 16:41:24 +0000

    Строю граф наследования

--- trunk/Container/src/container/Container.java
+++ trunk/Container/src/container/Container.java
@@ -1,28 +1,31 @@
 package container;
 
+import classloader.IPathsFilter;
 import classloader.JRoboClassLoader;
 import classloader.Resolver;
+import exceptions.JRoboContainerException;
 import storage.IStorage;
 import storage.Storage;
-import exceptions.JRoboContainerException;
 
 public class Container implements IContainer {
 
 	private JRoboClassLoader classLoader;
 	private IStorage storage;
 	private Resolver resolver;
+    private IPathsFilter filter;
 
-	public Container() {
+	public Container(IPathsFilter filter) {
 		storage = new Storage();
 		resolver = new Resolver();
-		classLoader = new JRoboClassLoader(storage, System.getProperty("java.class.path"));
+		classLoader = new JRoboClassLoader(storage, System.getProperty("java.class.path"), filter);
 		classLoader.loadClasses();
+        storage.buildFullDiagram();
 	}
 	
 	@SuppressWarnings("unchecked")
 	@Override
-	public <T> T get(Class<T> requiredInterface) throws JRoboContainerException {
-		Class<?> resolvedClass = resolver.resolveClass(requiredInterface, storage.getImplementations(requiredInterface));
+	public <T> T get(Class<T> requiredAbstraction) throws JRoboContainerException {
+		Class<?> resolvedClass = resolver.resolveClass(requiredAbstraction, storage.getImplementations(requiredAbstraction));
 		if(storage.getInstance(resolvedClass) == null)
 			storage.putInstance(resolvedClass, resolver.createNewInstance(resolvedClass));
 		return (T)storage.getInstance(resolvedClass);
@@ -30,8 +33,8 @@ public class Container implements IContainer {
 
 	@SuppressWarnings("unchecked")
 	@Override
-	public <T> T create(Class<T> requiredInterface) throws JRoboContainerException {
-		Class<?> resolvedClass = resolver.resolveClass(requiredInterface, storage.getImplementations(requiredInterface));
+	public <T> T create(Class<T> requiredAbstraction) throws JRoboContainerException {
+		Class<?> resolvedClass = resolver.resolveClass(requiredAbstraction, storage.getImplementations(requiredAbstraction));
 		return (T)resolver.createNewInstance(resolvedClass);
 	}
 }
