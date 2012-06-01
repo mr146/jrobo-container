fe62be090f4821c7ad7b971edf371fdd81ba9e38
Den.Mukhametianov@gmail.com <Den.Mukhametianov@gmail.com@0c7f8088-1b77-78cd-0a42-4bde29f108f7>
2012-06-01 08:41:56 +0000

    NoSystemsFilter - теперь под виндой.

--- ContainerTests/src/tests/container/NoSystemsFilter.java
+++ ContainerTests/src/tests/container/NoSystemsFilter.java
@@ -7,6 +7,6 @@ public class NoSystemsFilter implements IPathsFilter
 
     @Override
     public boolean accept(String path) {
-        return !path.startsWith("/System");
+        return !path.startsWith("/System") && !path.startsWith("C:\\Program Files");
     }
 }
