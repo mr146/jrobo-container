c2cbf11ebbc3100cc5b365e019328ffbb9b3a477
Den.Mukhametianov@gmail.com <Den.Mukhametianov@gmail.com@0c7f8088-1b77-78cd-0a42-4bde29f108f7>
2012-08-25 15:39:07 +0000

    bind'ы

--- Container/src/container/Container.java
+++ Container/src/container/Container.java
@@ -24,6 +24,8 @@ public class Container implements IContainer {
 	@SuppressWarnings("unchecked")
 	@Override
 	public <T> T get(Class<T> requiredAbstraction) throws JRoboContainerException {
+        if(storage.hasInstance(requiredAbstraction))
+            return (T)storage.getInstance(requiredAbstraction);
         Class<?> resolvedClass = resolveClass(requiredAbstraction);
 		if(storage.getInstance(resolvedClass) == null)
 			storage.putInstance(resolvedClass, resolver.createNewInstance(resolvedClass));
@@ -40,4 +42,10 @@ public class Container implements IContainer {
 		Class<?> resolvedClass = resolveClass(requiredAbstraction);
 		return (T)resolver.createNewInstance(resolvedClass);
 	}
+
+    @Override
+    public <T1, T2 extends T1> void bind(Class<T1> abstraction, T2 instance) {
+        storage.bindInstance(abstraction, instance);
+    }
+
 }
